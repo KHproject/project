f21aca9fc466b4157617d348e4ee01388ee8cb2b
user1 <user1@KH1>
2016-04-28 13:40:47 +0900

    아 졸려

---
+++ donjom/WebContent/manager/manager_main.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>manager page</title>
+</head>
+<body>
+	<form method="post" action="manager_member.dj">
+	<input type="submit" value="회원 검색"/>
+	
+	<input type="button" value="카테고리 추가" onclick="window.location.href=">
+	<input type="button" value="대출 심사" onclick="window.location.href=">
+	</form>
+</body>
+</html>
--- donjom/src/manager/bean/ManagerBean.java
+++ donjom/src/manager/bean/ManagerBean.java
@@ -1,4 +1,4 @@
-package main.bean;
+package manager.bean;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
@@ -7,19 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class MainBean {
+public class ManagerBean {
 	
 	@Autowired
 	private SqlMapClientTemplate sqlMap;
 	@Autowired
 	private ModelAndView mv;
 	
-	
-	
-	@RequestMapping("/main.dj")
-	public ModelAndView main(){
+	@RequestMapping("/manager_main.dj")
+	public ModelAndView managermain(){
 		
-		mv.setViewName("/main/main.jsp");
+		mv.setViewName("/manager/manager_main.jsp");
+		return mv;
+	}
+	@RequestMapping("/manager_member.dj")
+	public ModelAndView managermember(){
+		
+		mv.setViewName("/manager/manager_member.jsp");
 		return mv;
 	}
 
